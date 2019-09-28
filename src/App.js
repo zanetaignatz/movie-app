0419932c36e6312c57c2739fe03689168788a8aa
Bart <korysiu@gmail.com>
2019-09-28 18:07:53 +0200

    Added footer, color and RWD

--- src/App.js
+++ src/App.js
@@ -7,18 +7,18 @@ class App extends Component {
     return (
       <div className="App container-fluid" >
         <div className="App-header row container-fluid">
-          <div className="logo-container col-6 col-md- row"> 
-            <img src={tmdbLogo} className="logo col-6 col-md- " alt="TMDB logo" height="50"/>
-            <h1 className="App-title col-4 col-md- ">MovieApp</h1>
+          <div className="logo-container col-6 row"> 
+            <img src={tmdbLogo} className="logo col-6" alt="TMDB logo" height="50"/>
+            <h1 className="App-title col-4">MovieApp</h1>
           </div>
-          <div className="input-container col-6 col-md- ">
+          <div className="input-container col-6">
             
           </div>
         </div>
         <div className ="MovieCard col-12 col-md-12">
 
-          <div className="meta-data-container row col-12 col-md-8 col-lg-7">
-            <div className="poster-container col-11 col-md-4 col-lg-5 ">
+          <div className="meta-data-container row col-12 col-md-10 col-lg-9">
+            <div className="poster-container col-11 col-md-5 col-lg-5 ">
               <img className='poster' alt="film poster" src="https://image.tmdb.org/t/p/w1280/i91mfvFcPPlaegcbOyjGgiWfZzh.jpg"/>
             </div>
             <div className="info-container col">
@@ -29,10 +29,10 @@ class App extends Component {
                 <span className="genre-list">Historyczny, Dramat</span>
                 <div className="row release-details">
                   <div className="col-6"> Director: <span className="meta-data">Damien Chazelle</span></div>
-                  <div className="col-6"> Vote Average: <span className="meta-data">7,1</span></div>
+                  <div className="col-6"> Vote Average: <span className="meta-data">7,1 / 10</span></div>
                 </div>
                 <div className="row release-details">  
-                  <div className="col-6"> Original Release: <span className="meta-data">12 października 2018</span></div>
+                  <div className="col-6"> Original Release: <span className="meta-data">12-10-2018</span></div>
                   <div className="col-6"> Running Time: <span className="meta-data">144 min</span> </div>
                 </div>
               </div>
