eb6123d54974044954dbfc968c5294119829f008
olgasmoczynska <o.smoczynska@gmail.com>
2019-10-03 13:45:56 +0200

    added API handler and loaded movies on mount using searchbar value

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,35 +1,60 @@
 import React from 'react';
 import tmdbLogo from '../powered-by-blue.svg';
+import MovieAPI from '../handlers/api';
 
 class SearchBar extends React.Component {
 
+    constructor(props) {
+        super(props)
+        this.api = new MovieAPI();
+        this.state = {
+            value: ''
+        };
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(e){
+    }
+    
+    async loadMovies() {
+        let movies = await this.api.searchMovies(this.state.value);
+        this.state.movies = movies;
+    }
+
+    componentDidMount() {
+        this.loadMovies();
+    }
+
     render () {
+        var imgStyle = {
+            height: '50px'
+        }
 
-var imgStyle = {
-    height: '50px'
-}
-
-    return (
-        <div className="App-header row container-fluid align-items-center"> 
-            <div className="logo-container col-12 col-sm-6 col-md-7 col-lg-6 row px-0 px-md-auto">
-                <img src={tmdbLogo} className="logo col-3 col-sm-3 col-md-6 mx-auto mx-sm-0" alt="TMDB logo"
-                style={imgStyle} />
-                <h1 className="App-title col-12 col-sm-3 px-5 mb-0 text-center">MovieApp</h1>
-            </div>
-            <div className="input-container col-10 col-sm-8 col-md-5 col-lg-6 mx-auto pt-md-4 align-items-center">
-              <div className="dropdown active-green-3 active-green-4 mb-4 col-md-10 col-lg-7">               
-                <input id="dropdownMenuInput" className="form-control dropdown-toggle" type="text"
-                 placeholder="Search..." aria-label="Search" data-toggle="dropdown"
-                 aria-haspopup="true" aria-expanded="true"/>
-                 <div className="dropdown-menu" aria-labelledby="dropdownMenuInput">
-                    <a className="dropdown-item" href="#">Forrest Gump</a>
-                    <a className="dropdown-item" href="#">Ojciec Chrzestny</a>
-                    <a className="dropdown-item" href="#">Pulp Fiction</a>
+        return (
+            <div className="App-header row container-fluid align-items-center"> 
+                <div className="logo-container col-12 col-sm-6 col-md-7 col-lg-6 row px-0 px-md-auto">
+                    <img src={tmdbLogo} className="logo col-3 col-sm-3 col-md-6 mx-auto mx-sm-0" alt="TMDB logo"
+                    style={imgStyle} />
+                    <h1 className="App-title col-12 col-sm-3 px-5 mb-0 text-center">MovieApp</h1>
                 </div>
-              </div>
-            </div>
+                <div className="input-container col-10 col-sm-8 col-md-5 col-lg-6 mx-auto pt-md-4 align-items-center">
+                <div className="dropdown active-green-3 active-green-4 mb-4 col-md-10 col-lg-7">               
+                    <input id="dropdownMenuInput" className="form-control dropdown-toggle" type="text"
+                    placeholder="Search..." aria-label="Search" data-toggle="dropdown"
+                    aria-haspopup="true" aria-expanded="true"
+                    value={this.state.value} 
+                    onChange={this.handleChange}
 
-        </div>
+                    />
+                    <div className="dropdown-menu" aria-labelledby="dropdownMenuInput">
+                        <a className="dropdown-item" href="#">Forrest Gump</a>
+                        <a className="dropdown-item" href="#">Ojciec Chrzestny</a>
+                        <a className="dropdown-item" href="#">Pulp Fiction</a>
+                    </div>
+                </div>
+                </div>
+
+            </div>
      
              );  
           
---
+++ src/handlers/api.js
@@ -0,0 +1,25 @@
+import axios from 'axios';
+
+export default class MovieAPI {
+    constructor() {
+        this.baseURL = 'https://api.themoviedb.org/3/';
+        this.apiKey = '488bca8d82d3a3bcf62806a18763ac9f';
+    }
+
+    async searchMovies(query) {
+        let result;
+        await axios.get(`${this.baseURL}search/movie?api_key=${this.apiKey}&query=${query}`)
+            .then(res => {
+                result = res.data.results;
+            });
+        return result;
+    }
+    async getMovie(movieID) {
+        let result;
+        await axios.get(`${this.baseURL}movie/${movieID}?api_key=${this.apiKey}`)
+        .then(res => {
+            result = res.data;
+        });
+        return result;
+    }
+}
