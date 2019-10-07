5330fd66240aa523363c4908c219391d62cb53b6
Zaneta <zaneta.ignatz@o2.pl>
2019-10-07 11:40:48 +0200

    Updates

---
+++ precache-manifest.a8ca4f838615c2942235e6a7abe32dfb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cdb04538ae45cc1685a1a4b643b36a84",
+    "url": "/movie-app/index.html"
+  },
+  {
+    "revision": "ecb8abc8e9e205f25b86",
+    "url": "/movie-app/static/css/main.c56d1b04.chunk.css"
+  },
+  {
+    "revision": "dbd35699a65153d3a60f",
+    "url": "/movie-app/static/js/2.d347e66a.chunk.js"
+  },
+  {
+    "revision": "ecb8abc8e9e205f25b86",
+    "url": "/movie-app/static/js/main.34c75230.chunk.js"
+  },
+  {
+    "revision": "a13893a554cfe774cd55",
+    "url": "/movie-app/static/js/runtime-main.06a39056.js"
+  },
+  {
+    "revision": "5dacd004927da04b74e271b176c3dee0",
+    "url": "/movie-app/static/media/gplaypattern.5dacd004.png"
+  },
+  {
+    "revision": "c74849c081ea43b27c5f9e57c0f2f711",
+    "url": "/movie-app/static/media/powered-by-blue.c74849c0.svg"
+  }
+]);
